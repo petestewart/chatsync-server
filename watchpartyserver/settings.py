788ca9058abf1000e5b7e92bd5337f8e0733de3a
Pete Stewart <62906411+petestewart@users.noreply.github.com>
2020-12-09 14:34:04 -0600

    Update settings.py

--- watchpartyserver/settings.py
+++ watchpartyserver/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zomkzdxo(a7!d2@2t#_m@vo@hphtki7^p-9b5x6($ufusxxpp#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
